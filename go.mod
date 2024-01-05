f0964d5aec991da29d9e5971cce3caa149d4f47a
AsmanovLev <asmanovlev@gmail.com>
2024-01-06 00:22:09 +0700

    copypaste cgofuse helloworld

--- go.mod
+++ go.mod
@@ -27,6 +27,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/winfsp/cgofuse v1.5.0 // indirect
 	go.opentelemetry.io/otel v1.21.0 // indirect
 	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"log"
 	"os"
@@ -15,6 +16,7 @@ type Config struct {
 }
 
 func main() {
+
 	log.Println("Reading config from config.yml...")
 	yamlFile, err := os.ReadFile("config.yml")
 	if err != nil {
@@ -40,4 +42,72 @@ func main() {
 	downloaded := MTg.TgDownload(tgc, document)
 
 	log.Println("Downloaded following string:", string(downloaded))
+}
+*/
+
+import (
+	"fmt"
+	"github.com/winfsp/cgofuse/fuse"
+	"os"
+)
+
+const (
+	filename = "hello"
+	contents = "hello, world\n"
+)
+
+type Hellofs struct {
+	fuse.FileSystemBase
+}
+
+func (self *Hellofs) Open(path string, flags int) (errc int, fh uint64) {
+	switch path {
+	case "/" + filename:
+		return 0, 0
+	default:
+		return -fuse.ENOENT, ^uint64(0)
+	}
+}
+
+func (self *Hellofs) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) {
+	fmt.Println("someone read attributes")
+	switch path {
+	case "/":
+		stat.Mode = fuse.S_IFDIR | 0555
+		return 0
+	case "/" + filename:
+		stat.Mode = fuse.S_IFREG | 0444
+		stat.Size = int64(len(contents))
+		return 0
+	default:
+		return -fuse.ENOENT
+	}
+}
+
+func (self *Hellofs) Read(path string, buff []byte, ofst int64, fh uint64) (n int) {
+	endofst := ofst + int64(len(buff))
+	if endofst > int64(len(contents)) {
+		endofst = int64(len(contents))
+	}
+	if endofst < ofst {
+		return 0
+	}
+	n = copy(buff, contents[ofst:endofst])
+	return
+}
+
+func (self *Hellofs) Readdir(path string,
+	fill func(name string, stat *fuse.Stat_t, ofst int64) bool,
+	ofst int64,
+	fh uint64) (errc int) {
+	fill(".", nil, 0)
+	fill("..", nil, 0)
+	fill(filename, nil, 0)
+	return 0
+}
+
+func main() {
+	hellofs := &Hellofs{}
+	host := fuse.NewFileSystemHost(hellofs)
+	host.Mount("", os.Args[1:])
 }
